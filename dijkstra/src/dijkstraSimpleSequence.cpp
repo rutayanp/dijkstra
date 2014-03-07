557b0518105d368a0171e80c714b6141ce9221d4
rutayanp <rutayanp@gmail.com>
2014-03-07 01:20:17 -0800

    simple sequence

--- dijkstra/src/dijkstraSimpleSequence.cpp
+++ dijkstra/src/dijkstraSimpleSequence.cpp
@@ -7,16 +7,39 @@
 *============================================================================*/
 
 #include <iostream>
+#include <string.h>
+
 using namespace std;
 
 enum state { visited, unvisited };
 
 int * generateGraph(int n){
+	int graph[81] = {0, 4, 0, 0, 0, 0, 0, 8, 0,
+			         4, 0, 8, 0, 0, 0, 0, 11, 0,
+	                 0, 8, 0, 7, 0, 4, 0, 0, 2,
+	                 0, 0, 7, 0, 9, 14, 0, 0, 0,
+	                 0, 0, 0, 9, 0, 10, 0, 0, 0,
+	                 0, 0, 4, 0, 10, 0, 2, 0, 0,
+	                 0, 0, 0, 14, 0, 2, 0, 1, 6,
+	                 8, 11, 0, 0, 0, 0, 1, 0, 7,
+	                 0, 0, 2, 0, 0, 0, 6, 7, 0};
 
+	return graph;
 }
 
-int removeMin(int *dist, state *isvisited){
+int removeMin(int *dist, state *isvisited, int n){
 
+	int min = INT_MAX;
+	int min_index;
+
+	for(int i = 0; i < n; i++){
+		if (dist[i] < min && isvisited[i] == unvisited){
+			min = dist[i];
+			min_index = i;
+		}
+	}
+	cout << min << endl;
+	return min_index;
 }
 
 void stpSimpleSequence(int n, int x){
@@ -34,10 +57,8 @@ void stpSimpleSequence(int n, int x){
 	dist[x] = 0;
 
 	for(int i = 0; i < n; i++){
-
-		u = removeMin(dist, isvisited); //implementation required
+		u = removeMin(dist, isvisited, n); //implementation required
 		isvisited[u] = visited;
-
 		for(int v = 0; v < n; v++){
 			if((graph[u * n + v] != 0) && (isvisited[v] == unvisited) && (dist[v] >= (dist[u] + graph[u * n + v]))){
 				dist[v] = dist[u] + graph[u * n + v];
@@ -51,7 +72,19 @@ void stpSimpleSequence(int n, int x){
 	}
 }
 
-int main() {
-	cout << "trial" << endl; // prints trial
+int main(int argc, char *argv[]) {
+
+	int n;
+	int d;
+	int x; //starting node
+	/*if(strcmp(argv[1], "-r") == 0){
+
+	} else if (strcmp(argv[1], "-s")){
+
+	} else if (strcmp(argv[1], "-f")){
+
+	}*/
+
+	stpSimpleSequence(9, 0);
 	return 0;
 }
